11b0f9de5bc979689f8c0e775d92b9a2200aed2b
venutti <lvenutti@fi.uba.ar>
2022-09-05 14:33:09 -0300

    Código refactorizado
    Se cambió la idea de usar una COLA, para usar variables globales y hacer más fácil el entendimiento del código

--- main.js
+++ main.js
@@ -18,123 +18,107 @@ function substract(a, b) {
 }
 
 /*DISPLAY FUNCTIONS*/
-function displayNumber(e) {
-    const number = e.target.textContent;
-    const digits = document.querySelector("#display #digits");
-    if (!actualDigits) {
-        digits.textContent = number;
-    } else {
-        const numberList = actualDigits.split("");
-        // 9 digitos max
-        if (numberList.length === 9) return
-        numberList.push(number);
-        digits.textContent = numberList.join("");
-    }
+function appendNumber(number) {
+    const maxDigits = 9;
+    if (currentOperand.length === maxDigits) return
+    currentOperand = currentOperand.toString() + number.toString();
 }
-function getNumberDisplay() {
-    let digits = actualDigits;
-    if (!actualDigits) {
-        digits = (queue[0]) ? queue[0] : 0;
-    }
-    const sign = document.querySelector("#display #sign").textContent;
-    return (sign) ? -(+digits) : (+digits);
+function changeSign() {
+    currentOperand = (+currentOperand * (-1)).toString();
 }
-function clearDisplay() {
-    const digits = document.querySelector("#display #digits");
-    digits.textContent = "0";
+function clearAll() {
+    currentOperand = "";
+    previousOperand = "";
+    operation = undefined;
 }
 function clearDigit() {
-    const digits = document.querySelector("#display #digits");
-    const numberList = digits.textContent.split("");
-    numberList.pop();
-    digits.textContent = (numberList.length === 0) ? "0" : numberList.join("");
+    currentOperand = currentOperand.toString().slice(0, -1);
 }
-function clearActualDigits() {
-    actualDigits = "";
+function chooseOperation(newOperation) {
+    console.log(newOperation);
+    if (currentOperand === "") return;
+    if (previousOperand !== "") compute();
+    operation = newOperation;
+    previousOperand = currentOperand;
+    currentOperand = "";
 }
-function showResult(result) {
-    const digits = document.querySelector("#display #digits");
-    if (result === NaN) {
-        digits.textContent = "lol";
-        return
-    }
-    let resultString = result.toString();
-    if (resultString.length > 9) {
-        console.log(resultString);
-        resultString = Number.parseFloat(resultString).toExponential(6);
-    }
-    digits.textContent = resultString;
-}
-
-/*EVALUATE FUNCTIONS*/
-function evaluateOperand(e) {
-    const operation = {
-        "+/-" : changeSign,
+function compute() {
+    const operationDict = {
         "*" : multiply,
         "/": divide,
         "+": sum,
         "-": substract,
-        "=": executeQueue,
     }
-    const operator = this.textContent;
-    if (operator === "+/-") {
-        operation[operator]();
-        return;
-    }
-    const operand = getNumberDisplay();
-    queueAdd(operand);
-    if (operator === "=") {
-        operation[operator]();
-        return;
-    }
-    clearActualDigits();
-    evaluateQueue();
-    queueAdd(operation[operator]);
+    let computation;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    console.log(previousOperand, operation, currentOperand);
+    computation = operationDict[operation](prev, current);
+    computation = getResult(computation);
+    currentOperand = computation;
+    operation = undefined;
+    previousOperand = "";
+}
+function updateDisplay() {
+    displayElement.innerText = currentOperand;
 }
 
-/*QUEUE FUNCTIONS*/
-function queueAdd(elem) {
-    queue.push(elem);
-}
-function evaluateQueue() {
-    if (queue.length === 3) executeQueue();
-}
-function executeQueue() {
-    console.log(queue);
-    let result;
-    //0 o 1 elemento --> vaciar cola
-    //2 o 3 elementos --> resolver, mostrar, vaciar cola
-    const operandA = queue[0];
-    const operator = queue[1];
-    const operandB = (queue[2]) ? queue[2] : queue[0];
-    if (queue.length >= 2) { // 2 o 3
-        result = operator(operandA, operandB);
+function getResult(result) {
+    let resultString = result.toString();
+    let integerPart = resultString.split(".")[0] || "";
+    let decimalPart = resultString.split(".")[1] || "";
+    if (integerPart.length > 9) {
+        resultString = Number.parseFloat(resultString).toExponential(6);
+    } else if (decimalPart.length > 9) {
+        resultString = resultString.slice(0, 9);
     }
-    clearQueue();
-    showResult(result);
-}
-function clearQueue() {
-    const times = queue.length;
-    for (let i = 0; i < times; i++) {
-        queue.pop();
-    }
+    return resultString;
 }
 
-const queue = [];
-// para que, cuando apreto un operador, no se borre
-// el anterior numero en pantalla
-// y funcione como las calculadoras reales
-let actualDigits = "";
 
+const displayElement = document.querySelector("#display");
 
-const numbers = document.querySelectorAll(".button.number");
-numbers.forEach(number => number.addEventListener("click", displayNumber));
-const ac = document.querySelector("#ac");
-ac.addEventListener("click", clearDisplay);
-const c = document.querySelector("#c");
-c.addEventListener("click", clearDigit);
-const operators = document.querySelectorAll(".button.oper");
-operators.forEach(operator => operator.addEventListener("click", evaluateOperand));
+let previousOperand = "";
+let operation = undefined;
+let currentOperand = "";
+
+const numberButtons = document.querySelectorAll(".button.number");
+const allClearButton = document.querySelector("#ac");
+const deleteButton = document.querySelector("#c");
+const operatorButtons = document.querySelectorAll(".button.oper");
+const equalsButton = document.querySelector("[data-equals]");
+const changeSignButton = document.querySelector("[data-change-sign]");
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        appendNumber(button.innerText);
+        updateDisplay();
+    });
+});
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        chooseOperation(button.innerText);
+        updateDisplay();
+    });
+});
+equalsButton.addEventListener("click", () => {
+    compute();
+    updateDisplay();
+});
+changeSignButton.addEventListener("click", () => {
+    changeSign();
+    updateDisplay();
+});
+allClearButton.addEventListener("click", () => {
+    clearAll();
+    updateDisplay();
+});
+deleteButton.addEventListener("click", () => {
+    clearDigit();
+    updateDisplay();
+});
+
 //problema -> no quiero borrar el display cuando apreto un operador
 /*
 propuesta -> cada vez que toco un operador (+,-,*,/)
